e4651ab62431dd89dbf7aa2745c6daa6643f6c22
sincerelyyyash <yta0088@gmail.com>
2024-03-02 21:32:25 +0530

    NextJS class

---
+++ week-14/nextjs-class/next-app/app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+
+export default function({children}:{children: React.ReactNode}){
+    return <div
+    className="border-b text-center p-1"
+    >20% off for the next 3 days
+    {children}
+    </div>
+}
---
+++ week-14/nextjs-class/next-app/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+import { SignInComponent } from "@/components/Signin";
+
+export default function Signin() {
+    return <div>
+        <SignInComponent />
+    </div>
+}
+
