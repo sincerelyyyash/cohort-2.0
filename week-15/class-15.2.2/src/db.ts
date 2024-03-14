70b8036d4bc1afca37e23a29d0c109a362701d16
sincerelyyyash <yta0088@gmail.com>
2024-03-15 02:29:27 +0530

    Docker-compose added

--- week-15/class-15.2.2/src/db.ts
+++ week-15/class-15.2.2/src/db.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const mongoUrl: string = 'mongodb://mongoClass:27017/myDatabase';
+const mongoUrl: string = process.env.MONGO_URL || 'mongodb://mongodb:27017/myDatabase';
 
 // Connect to MongoDB
 mongoose.connect(mongoUrl)
